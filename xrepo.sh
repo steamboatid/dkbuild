a87b483f6c724de96848fd32eb46f0871a7b08a2
randompage <steamboatid@gmail.com>
2021-09-13 04:29:41 +0700

    autoupdate dkbuild 2021-09-13-04:29:41

--- xrepo.sh
+++ xrepo.sh
@@ -15,7 +15,10 @@
 # 4. update Release, Release.gpg, InRelease, Packages (.xz,.bz2,.gz)
 # 5. publish
 
+
 mkdir -p /tb2/phideb/{dists,pool}/{buster,bullseye}
+mkdir -p /tb2/phideb/dists/{buster,bullseye}/main/binary-amd64
+
 
 rsync -aHAXvztr --numeric-ids --modify-window 5 --omit-dir-times --delete \
 /tb2/build/buster-all/ /tb2/phideb/pool/buster/
@@ -23,16 +26,33 @@ rsync -aHAXvztr --numeric-ids --modify-window 5 --omit-dir-times --delete \
 rsync -aHAXvztr --numeric-ids --modify-window 5 --omit-dir-times --delete \
 /tb2/build/bullseye-all/ /tb2/phideb/pool/bullseye/
 
-cd /tb2/phideb; \
-apt-ftparchive --arch amd64 packages pool/buster/ > dists/buster/Packages
 
 cd /tb2/phideb; \
-apt-ftparchive --arch amd64 packages pool/bullseye/ > dists/bullseye/Packages
+apt-ftparchive --arch amd64 packages pool/buster/ > dists/buster/main/binary-amd64/Packages
+
+cd /tb2/phideb; \
+apt-ftparchive --arch amd64 packages pool/bullseye/ > dists/bullseye/main/binary-amd64/Packages
+
+
+cd /tb2/phideb/dists/buster/main/binary-amd64; \
+gzip -kf Packages; apt-ftparchive release . > Release
+
+cd /tb2/phideb/dists/bullseye/main/binary-amd64; \
+gzip -kf Packages; apt-ftparchive release . > Release
+
+
+cd /tb2/phideb/dists/buster/main; \
+gzip -kf Packages; apt-ftparchive release . > Release
+
+cd /tb2/phideb/dists/bullseye/main; \
+gzip -kf Packages; apt-ftparchive release . > Release
+
 
 cd /tb2/phideb/dists/buster; \
 gzip -kf Packages; apt-ftparchive release . > Release
 
 cd /tb2/phideb/dists/bullseye; \
 gzip -kf Packages; apt-ftparchive release . > Release
+
 
 cd /tb2/phideb/dists; apt-ftparchive release . > Release
---
+++ ztest.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+
+export DEBIAN_FRONTEND="noninteractive"
+
+export DEBFULLNAME="Dwi Kristianto"
+export DEBEMAIL="steamboatid@gmail.com"
+export EMAIL="steamboatid@gmail.com"
+
+export RELNAME=$(lsb_release -sc)
+export RELVER=$(LSB_OS_RELEASE="" lsb_release -a 2>&1 | grep Release | awk '{print $2}' | tail -n1)
+
+
+echo \
+'Acquire::Queue-Mode "host";
+Acquire::Languages "none";
+Acquire::http { Pipeline-Depth "200"; };
+Acquire::https { Verify-Peer false; };
+'>/etc/apt/apt.conf.d/99translations
+
+
+echo \
+'APT::Get::AllowUnauthenticated "true";
+Acquire::Check-Valid-Until "false";
+Acquire::AllowDowngradeToInsecureRepositories "true";
+Acquire::AllowInsecureRepositories "true";
+APT::Ignore "gpg-pubkey";
+Acquire::ForceIPv4 "true";
+
+APT::Authentication::TrustCDROM "true";
+Acquire::cdrom::mount "/media/cdrom";
+Dir::Media::MountPath "/media/cdrom";
+
+APT::Get::Install-Recommends "false";
+APT::Get::Install-Suggests "false";
+
+APT::Install-Recommends "false";
+APT::Install-Suggests "false";
+
+Binary::apt::APT::Keep-Downloaded-Packages "true";
+APT::Keep-Downloaded-Packages "true";
+
+Acquire::EnableSrvRecords "false";
+APT::FTPArchive::AlwaysStat "false";
+
+APT::Cache-Limit "100000000";
+'>/etc/apt/apt.conf.d/98more
+
+echo \
+'export HISTFILESIZE=100000
+export HISTSIZE=100000
+
+#-- encodings
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+'>/etc/environment && cat /etc/environment
+
+export PATH=$PATH:/usr/sbin
+timedatectl set-timezone Asia/Jakarta
+
+
+echo \
+'deb [trusted=yes] http://repo.aisits.id/phideb ${RELNAME} ./
+deb [trusted=yes] http://repo.aisits.id/phideb ${RELNAME} ./
+'>/etc/apt/sources.list.d/nginx-ppa-devel.list
+
+
+cd `mktemp -d`; \
+apt update;\
+dpkg --configure -a; \
+apt install -yf locales dialog apt-utils lsb-release apt-transport-https ca-certificates \
+gnupg2 apt-utils tzdata curl && \
+echo 'en_US.UTF-8 UTF-8'>/etc/locale.gen && locale-gen &&\
+apt-key adv --fetch-keys http://repo.aisits.id/trusted-keys &&\
+apt update; apt full-upgrade -fy
