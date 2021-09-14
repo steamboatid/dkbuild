17cb925bf156821a78fb5539b851db158de1b83d
randompage <steamboatid@gmail.com>
2021-09-15 00:13:46 +0700

    autoupdate dkbuild 2021-09-15-00:13:46

--- xrepo.sh
+++ xrepo.sh
@@ -62,3 +62,14 @@ cd /tb2/phideb/dists/buster
 
 cd /tb2/phideb/dists/bullseye
 /bin/bash /tb2/build/xrelease.sh bullseye > Release
+
+
+printf " chown folders \n"
+find -L /w3repo/phideb -type d -group root  -exec chown webme:webme {} \;
+find -L /w3repo/phideb -type d -user root  -exec chown webme:webme {} \;
+
+printf " chown files \n"
+find -L /w3repo/phideb -type f -group root  -exec chown webme:webme {} \;
+find -L /w3repo/phideb -type f -user root  -exec chown webme:webme {} \;
+
+/bin/bash /root/cf-clear.sh
