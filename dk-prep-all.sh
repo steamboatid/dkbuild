c20aba1752c0f150460cba039bf0de9cc9c84c7f
randompage <steamboatid@gmail.com>
2021-09-14 18:34:42 +0700

    autoupdate dkbuild 2021-09-14-18:34:42

--- dk-prep-all.sh
+++ dk-prep-all.sh
@@ -44,20 +44,28 @@ fix_keydb_permission_problem() {
 	# return if not installed yet
 	if [ `dpkg -l | grep keydb | grep -v "^ii" | wc -l` -lt 1 ]; then return 0; fi
 
+	>/var/log/keydb/keydb-sentinel.log
+	>/var/log/keydb/keydb-server.log
+
 	cd `mktemp -d`; \
 	systemctl stop redis-server; systemctl disable redis-server; systemctl mask redis-server; \
 	systemctl daemon-reload; apt remove -y redis-server
 	mkdir -p /var/lib/keydb /var/log/keydb /var/run/keydb /run/keydb; \
 	chown keydb.keydb -Rf /var/lib/keydb /var/log/keydb /var/run/keydb /run/keydb; \
 	find /var/lib/keydb /var/log/keydb /var/run/keydb /run/keydb -type d -exec chmod 775 {} \; ; \
-	find /var/lib/keydb /var/log/keydb /var/run/keydb /run/keydb -type d -exec chmod 664 {} \;
+	find /var/lib/keydb /var/log/keydb /var/run/keydb /run/keydb -type f -exec chmod 664 {} \;
+
+	# force modify config file
 	sed -i "s/^bind 127.0.0.1 \:\:1/\#-- bind 127.0.0.1 \:\:1\nbind 127.0.0.1/g" /etc/keydb/keydb.conf
 	sed -i "s/^logfile \/var/#-- logfile \/var/g" /etc/keydb/keydb.conf
-	sed -i "s/^dir \/var/#-- dir \/var/g" /etc/keydb/keydb.conf
 	sed -i "s/^dbfilename /#-- dbfilename /g" /etc/keydb/keydb.conf
 	sed -i "s/^save 900 /#-- save 900 /g" /etc/keydb/keydb.conf
 	sed -i "s/^save 300 /#-- save 300 /g" /etc/keydb/keydb.conf
 	sed -i "s/^save 60 /#-- save 60 /g" /etc/keydb/keydb.conf
+
+	# force modify config file
+	sed -i "s/^logfile \/var/#-- logfile \/var/g" /etc/keydb/sentinel.conf
+	sed -i "s/^dir \/var/#-- dir \/var/g" /etc/keydb/sentinel.conf
 }
 
 purge_pending_installs() {
