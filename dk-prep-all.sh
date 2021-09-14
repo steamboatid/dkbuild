f7bd4f72d5c0e626567ea42d34b9d5087288908f
randompage <steamboatid@gmail.com>
2021-09-14 17:58:41 +0700

    autoupdate dkbuild 2021-09-14-17:58:41

--- dk-prep-all.sh
+++ dk-prep-all.sh
@@ -40,6 +40,26 @@ get_update_new_git(){
 	update_old_git $DST
 }
 
+fix_keydb_permission_problem() {
+	# return if not installed yet
+	if [ `dpkg -l | grep keydb | wc -l` -lt 1 ]; then return 0; fi
+
+	cd `mktemp -d`; \
+	systemctl stop redis-server; systemctl disable redis-server; systemctl mask redis-server; \
+	systemctl daemon-reload; apt remove -y redis-server
+	mkdir -p /var/lib/keydb /var/log/keydb /var/run/keydb /run/keydb; \
+	chown keydb.keydb -Rf /var/lib/keydb /var/log/keydb /var/run/keydb /run/keydb; \
+	find /var/lib/keydb /var/log/keydb /var/run/keydb /run/keydb -type d -exec chmod 775 {} \; ; \
+	find /var/lib/keydb /var/log/keydb /var/run/keydb /run/keydb -type d -exec chmod 664 {} \;
+	sed -i "s/^bind 127.0.0.1 \:\:1/\#-- bind 127.0.0.1 \:\:1\nbind 127.0.0.1/g" /etc/keydb/keydb.conf
+	sed -i "s/^logfile \/var/#-- logfile \/var/g" /etc/keydb/keydb.conf
+	sed -i "s/^dir \/var/#-- dir \/var/g" /etc/keydb/keydb.conf
+	sed -i "s/^dbfilename /#-- dbfilename /g" /etc/keydb/keydb.conf
+	sed -i "s/^save 900 /#-- save 900 /g" /etc/keydb/keydb.conf
+	sed -i "s/^save 300 /#-- save 300 /g" /etc/keydb/keydb.conf
+	sed -i "s/^save 60 /#-- save 60 /g" /etc/keydb/keydb.conf
+}
+
 shopt -s expand_aliases
 alias aptold='apt -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"'
 
