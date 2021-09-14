85060a45b70c17e869ea9d3f84e0d4972885e435
randompage <steamboatid@gmail.com>
2021-09-14 12:27:55 +0700

    autoupdate dkbuild 2021-09-14-12:27:55

--- dk-fix-base-files.sh
+++ dk-fix-base-files.sh
@@ -12,30 +12,53 @@ export RELVER=$(LSB_OS_RELEASE="" lsb_release -a 2>&1 | grep Release | awk '{pri
 
 export TODAY=$(date +%Y%m%d-%H%M)
 
-find /var/lib/apt/lists/ -type f -delete; \
-find /var/cache/apt/ -type f -delete; \
-rm -rf /var/cache/apt/* /var/lib/dpkg/lock /var/lib/dpkg/lock-frontend \
-/var/lib/dpkg/lock /var/lib/dpkg/lock-frontend /var/cache/debconf/ \
-/etc/apt/preferences.d/00-revert-stable \
-/var/cache/debconf/ /var/lib/apt/lists/* \
-/var/lib/dpkg/lock /var/lib/dpkg/lock-frontend /var/cache/debconf/; \
-mkdir -p /root/.local/share/nano/ /root/.config/procps/
-apt clean
-apt update
-dpkg --configure -a
 
-cd `mktemp -d`; \
-apt purge --auto-remove --purge -fy \
-nginx* keydb* nutcracker* php* apache2* rsyslog* unattended-upgrades apparmor \
-anacron msttcorefonts ttf-mscorefonts-installer needrestart lua*dev lib*dev php*dev \
-xserver* xorg* x11*
+clean_apt_cache() {
+	find /var/lib/apt/lists/ -type f -delete; \
+	find /var/cache/apt/ -type f -delete; \
+	rm -rf /var/cache/apt/* /var/lib/dpkg/lock /var/lib/dpkg/lock-frontend \
+	/var/lib/dpkg/lock /var/lib/dpkg/lock-frontend /var/cache/debconf/ \
+	/etc/apt/preferences.d/00-revert-stable \
+	/var/cache/debconf/ /var/lib/apt/lists/* \
+	/var/lib/dpkg/lock /var/lib/dpkg/lock-frontend /var/cache/debconf/; \
+	mkdir -p /root/.local/share/nano/ /root/.config/procps/
+	apt clean
+	apt update
+	dpkg --configure -a
+}
 
-dpkg-query -Wf '${Package;-40}${Essential}\n' | grep yes | awk '{print $1}' > /tmp/ess
-dpkg-query -Wf '${Package;-40}${Priority}\n' | grep -E "required" | awk '{print $1}' >> /tmp/ess
-aptitude search ~E 2>&1 | awk '{print $2}' >> /tmp/ess
-aptitude search ~prequired -F"%p" >> /tmp/ess
-aptitude search ~pimportant -F"%p" >> /tmp/ess
+remove_non_base() {
+	cd `mktemp -d`; \
+	apt purge --auto-remove --purge -fy \
+	nginx* keydb* nutcracker* php* apache2* rsyslog* unattended-upgrades apparmor \
+	anacron msttcorefonts ttf-mscorefonts-installer needrestart lua*dev lib*dev php*dev \
+	xserver* xorg* x11*
 
-cat /tmp/ess | sort -u | sort | grep -v "apache2\|rsyslog\|nginx\|php\|keydb\|nutcracker"  | tr '\n' ' ' | \
-xargs apt install --reinstall --fix-missing --install-suggests \
---fix-broken  --allow-downgrades --allow-change-held-packages -fy
+	rm -rf /var/lib/keydb /var/log/keydb /var/run/keydb /run/keydb /usr/lib/php \
+	/etc/keydb /etc/nutcracker /etc/php /etc/nginx \
+	/lib/systemd/system/keydb* /etc/init.d/keydb* \
+	/lib/systemd/system/nutcracker* /etc/init.d/nutcracker* \
+	/lib/systemd/system/nginx* /etc/init.d/nginx* \
+	/lib/systemd/system/php* /etc/init.d/php*
+}
+
+reinstall_base() {
+	dpkg-query -Wf '${Package;-40}${Essential}\n' | grep yes | awk '{print $1}' > /tmp/ess
+	dpkg-query -Wf '${Package;-40}${Priority}\n' | grep -E "required" | awk '{print $1}' >> /tmp/ess
+	aptitude search ~E 2>&1 | awk '{print $2}' >> /tmp/ess
+	aptitude search ~prequired -F"%p" >> /tmp/ess
+	aptitude search ~pimportant -F"%p" >> /tmp/ess
+
+	cat /tmp/ess | sort -u | sort | grep -v "apache2\|rsyslog\|nginx\|php\|keydb\|nutcracker"  | tr '\n' ' ' | \
+	xargs apt install --reinstall --fix-missing --install-suggests \
+	--fix-broken  --allow-downgrades --allow-change-held-packages -fy
+}
+
+
+
+clean_apt_cache
+
+remove_non_base
+remove_non_base
+
+reinstall_base
