80c21345562a0d71465e93922dc9a7867a98bdda
randompage <steamboatid@gmail.com>
2021-09-16 15:56:02 +0700

    autoupdate dkbuild 2021-09-16-15:56:02

--- dk-prep-net.sh
+++ dk-prep-net.sh
@@ -100,9 +100,9 @@ grep -v "\-embed\|\-dbg\|dbgsym\|\-dev\|php5\|php7\|php8.1\|recode\|phalcon" |
 sed -E 's/\(([^()]*)\)//g' | sed -r 's/\s+//g' | sort -u | sort >> $FNOW1
 
 cd /root/org.src/php8
-cat $FNOW1 | sort -u | sort >> $FNOW2
+cat $FNOW1 | grep -v "recode\|phalcon" | sort -u | sort >> $FNOW2
 
-# echo "php-phalcon3" >> $FNOW2
+echo "php-phalcon3" >> $FNOW2
 cat $FNOW2 | tr "\n" " " | xargs apt build-dep -y --ignore-missing | tee $FSRC
 
 for apkg in $(cat $FSRC | cut -d" " -f2 | sed -r "s/'//g" | sort -u | sort); do
