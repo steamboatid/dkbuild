1329c58c2c7d8328aa795ac1af6f67ee5bbeb3e3
randompage <steamboatid@gmail.com>
2021-09-16 14:29:50 +0700

    autoupdate dkbuild 2021-09-16-14:29:50

--- dk-prep-net.sh
+++ dk-prep-net.sh
@@ -98,8 +98,9 @@ sed -E 's/\(([^()]*)\)//g' | sed -r 's/\s+//g' | sort -u | sort >> $FNOW1
 cd /root/org.src/php8
 cat $FNOW1 | sort -u | sort >> $FNOW2
 cat $FNOW2 | tr "\n" " " | xargs apt build-dep -y --ignore-missing | tee $FSRC
-cat $FSRC | cut -d" " -f2 | sed -r "s/'//g" | sort -u | sort | tr "\n" " " | \
-xargs apt source -y --ignore-missing
+for apkg in $(cat $FSRC | cut -d" " -f2 | sed -r "s/'//g" | sort -u | sort); do
+	apt source -y --ignore-missing $apkg
+done
 
 
 
