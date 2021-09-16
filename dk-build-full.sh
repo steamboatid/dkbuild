8132ac1f9ead84b9747fb0d59d74733525449547
randompage <steamboatid@gmail.com>
2021-09-16 14:49:39 +0700

    autoupdate dkbuild 2021-09-16-14:49:39

--- dk-build-full.sh
+++ dk-build-full.sh
@@ -67,7 +67,7 @@ time debuild --preserve-envvar=CCACHE_DIR --prepend-path=/usr/lib/ccache \
 --no-lintian --no-tgz-check --no-sign -b -uc -us -D 2>&1 | tee dkbuild.log
 
 isclang=$(cat dkbuild.log | grep -i clang | grep -i conflict | wc -l)
-isfail=$(cat dkbuild.log | grep -i failed)
+isfail=$(cat dkbuild.log | grep -i failed | wc -l)
 if [[ $isfail -gt 0 ]] && [[ $isclang -gt 0 ]]; then
 	# fakeroot debian/rules clean; \
 	export DEB_BUILD_PROFILES="noudep nocheck noinsttest"; \
