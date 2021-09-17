87da32abb0e26a7db940275879baaf06560bf301
randompage <steamboatid@gmail.com>
2021-09-18 01:14:09 +0700

    autoupdate dkbuild 2021-09-18-01:14:09

--- dk-purge-packages.sh
+++ dk-purge-packages.sh
@@ -7,7 +7,7 @@ printf "\n\n"
 pkgs=""
 for apkg in $(dpkg -l|grep "^ii"|sed -r "s/\s+/ /g"|cut -d" " -f2|sed -r "s/\:amd64//g"|grep -v linux); do
 	exists=$(grep -i "$apkg" /tb2/build/basic.pkgs | wc -l)
-	if [[ $exists -lt 0 ]]; then
+	if [[ $exists -lt 1 ]]; then
 		pkgs="${apkg} ${pkgs}"
 		printf " $apkg"
 	fi
