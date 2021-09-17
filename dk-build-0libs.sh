be0a0de4860e6358f844c0f4f990134a509b6bb6
randompage <steamboatid@gmail.com>
2021-09-17 18:52:18 +0700

    autoupdate dkbuild 2021-09-17-18:52:18

--- dk-build-0libs.sh
+++ dk-build-0libs.sh
@@ -59,7 +59,7 @@ check_build_log() {
 
 	printf "\n\n"
 	export TOTFAIL=0
-	for alog in $(find /root/src -iname "dkbuild.log" | sort -u); do
+	for alog in $(find /root/src -maxdepth 2 -type f -iname "dkbuild.log" | sort -u); do
 		NUMFAIL=$(tail -n100 ${alog} | grep "buildpackage" | grep failed | wc -l)
 		NUMSUCC=$(tail -n100 ${alog} | grep "buildpackage" | grep "binary-only upload" | wc -l)
 		if [[ $NUMSUCC -lt 1 ]] || [[ $NUMFAIL -gt 0 ]]; then
