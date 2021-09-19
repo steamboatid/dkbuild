6a91fe59168c74df719bec5ce33099d5a6cd4394
randompage <steamboatid@gmail.com>
2021-09-19 14:28:10 +0700

    autoupdate dkbuild 2021-09-19-14:28:10

--- dk-docker-install.sh
+++ dk-docker-install.sh
@@ -77,7 +77,7 @@ RUN git clone https://github.com/steamboatid/dkbuild /tb2/build &&\
 
 ">Dockerfile
 
+# --privileged=true \
 docker build --no-cache --network host \
---privileged=true \
 --volume /run/dbus/system_bus_socket:/run/dbus/system_bus_socket:ro \
 -t thedoc:latest -f ./Dockerfile  .
