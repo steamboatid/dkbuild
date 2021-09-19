80549fb9c649e99a8b0ffeea4f27a454b022ced6
randompage <steamboatid@gmail.com>
2021-09-19 21:05:54 +0700

    autoupdate dkbuild 2021-09-19-21:05:54

--- zargo.sh
+++ zargo.sh
@@ -5,7 +5,7 @@ rsync -aHAXvztr --numeric-ids --modify-window 5 --omit-dir-times \
 /tb2/build/*sh root@argo:/tb2/build/
 
 rsync -aHAXvztr --numeric-ids --modify-window 5 --omit-dir-times \
-/tb2/build/*sh root@abdi:/tb2/build/
+/tb2/build/*sh root@atan:/tb2/build/
 
 # nohup /bin/bash /tb2/build/zgit-auto.sh >/dev/null 2>&1 &
 /bin/bash /tb2/build/zgit-auto.sh
