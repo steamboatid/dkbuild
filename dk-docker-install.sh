48bd166c2bbbcf7d27f2d5aff88860a0b2eea4d7
randompage <steamboatid@gmail.com>
2021-09-19 16:09:26 +0700

    autoupdate dkbuild 2021-09-19-16:09:26

--- dk-docker-install.sh
+++ dk-docker-install.sh
@@ -64,18 +64,16 @@ ENV DEBIAN_FRONTEND=noninteractive
 ENV LANG='en_US.UTF-8 UTF-8' LANGUAGE='en_US.UTF-8 UTF-8' LC_ALL='en_US.UTF-8 UTF-8'
 
 WORKDIR /tb2
-RUN mkdir -p /tb2; echo 'nameserver 1.1.1.1' > /etc/resolv.conf; cat /etc/resolv.conf
-RUN ip a; ip r; ping 1.1.1.1 -c3; ping yahoo.com -c3
+RUN mkdir -p /tb2; echo 'nameserver 1.1.1.1' > /etc/resolv.conf; cat /etc/resolv.conf; \
+ip a; ip r; ping 1.1.1.1 -c3; ping yahoo.com -c3
 
 RUN printf '\
 deb http://repo.aisits.id/debian buster main contrib non-free \n\
 deb http://repo.aisits.id/debian-security buster/updates main contrib non-free \n\
 deb http://repo.aisits.id/debian buster-updates main contrib non-free \n\
 deb http://repo.aisits.id/debian buster-proposed-updates main contrib non-free \n\
-'>/etc/apt/sources.list
-
-ARG DEBIAN_FRONTEND=noninteractive
-RUN apt update; apt install -fy apt-utils; apt install -fy git locales systemd net-tools dnsutils
+'>/etc/apt/sources.list; \
+apt update; apt install -fy locales apt-utils; apt install -fy git net-tools dnsutils
 
 ENV LANG='en_US.UTF-8 UTF-8' LANGUAGE='en_US.UTF-8 UTF-8' LC_ALL='en_US.UTF-8 UTF-8'
 RUN git clone https://github.com/steamboatid/dkbuild /tb2/build &&\
