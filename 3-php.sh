e2f6db56cc74d47a1e52cf9d9cefdce1d4891de0
randompage <steamboatid@gmail.com>
2021-09-22 20:59:57 +0700

    autoupdate dkbuild 2021-09-22-20:59:57

--- 3-php.sh
+++ 3-php.sh
@@ -251,9 +251,12 @@ fi
 
 
 sleep 0.1
-sed -i "s/\-g//g" Makefile
+sed -i "s/\-g //g" Makefile
 sed -i "s/-O2/-O3/g" Makefile
 sed -i "s/noeneration-date/no-generation-date/g" Makefile
+sed -i "s/large-functionrowth/large-function-growth/g" Makefile
+sed -i "s/inline-unitrowth/inline-unit-growth/g" Makefile
+# sed -i "s///g" Makefile
 
 find /root/src/git-php/ -type d -name ".libs" -exec rm -rf {} \;  >/dev/null
 find /root/src/git-php/ -type d -name ".libs" -exec rm -rf {} \;  >/dev/null
@@ -269,6 +272,5 @@ else
 	printf "\n\n$doconf \n\n"
 	exit 0;
 fi
-
 
 sapi/cli/php -m
