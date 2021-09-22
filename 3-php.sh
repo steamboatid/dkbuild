0359bc5f0d84f0122ed1093f82586d5b7fa1b2d3
randompage <steamboatid@gmail.com>
2021-09-22 21:51:15 +0700

    autoupdate dkbuild 2021-09-22-21:51:15

--- 3-php.sh
+++ 3-php.sh
@@ -226,7 +226,7 @@ doconf="./configure \
 debops=$(cat debops)
 moreops=$(cat moreops)
 doconf="${doconf} ${debops} ${moreops}"
-doconf=$(printf "$doconf" | sed -r "s/\s/ \\\\ \n/g")
+doconf=$(printf "$doconf" | sed -r "s/\s+/ /g" | sed -r "s/\s/ \\\\\n/g")
 printf "\n\n$doconf \n\n"
 exit 0;
 
