4d7f7fcc20ef0f028ade12b33cb6940e0d20e40a
randompage <steamboatid@gmail.com>
2021-09-22 19:18:17 +0700

    autoupdate dkbuild 2021-09-22-19:18:17

--- 3-php.sh
+++ 3-php.sh
@@ -161,13 +161,11 @@ $(cat moreops) \
 2>&1 | tee dkconf.log
 
 
-bads=$(cat dkconf.log | grep -v "warning" | grep -i "mcrypt\|vips\|uuid\|gearman" | wc -l)
+bads=$(cat dkconf.log | grep -iv "warning" | grep -i "mcrypt\|vips\|uuid\|gearman" | wc -l)
 if [[ $bads -lt 1 ]]; then
 	printf "\n\n configure failed \n\n"
 	exit 0;
 fi
-cat dkconf.log | grep -v "warning" | grep -i "mcrypt\|vips\|uuid\|gearman"
-exit 0;
 
 
 if [[ $(tail -n30 dkconf.log | grep "Thank you for using PHP" | wc -l) -gt 0 ]]; then
