bd4997e63c9622cdff306ba0d23cda486620432d
randompage <steamboatid@gmail.com>
2021-09-25 04:29:08 +0700

    autoupdate dkbuild 2021-09-25-04:29:08

--- 4-php.sh
+++ 4-php.sh
@@ -183,6 +183,50 @@ dba_config = --disable-dba \
 	--disable-inifile \
 	--disable-flatfile
 
+common_EXTENSIONS += common
+common_config = --enable-bcmath \
+--enable-dom \
+--enable-fileinfo \
+--enable-gd \
+--enable-huge-code-pages \
+--enable-mbstring \
+--enable-opcache \
+--enable-opcache-jit \
+--enable-phar \
+--enable-posix \
+--enable-rtld-now \
+--enable-session \
+--enable-shmop \
+--enable-sigchild \
+--enable-soap \
+--enable-sockets \
+--enable-sysvmsg \
+--enable-sysvsem \
+--enable-sysvshm \
+--enable-tokenizer \
+--enable-xml \
+--enable-zts \
+--localstatedir=/var \
+--mandir=/usr/share/man \
+--with-curl \
+--with-external-pcre \
+--with-ffi \
+--with-gmp \
+--with-iconv \
+--with-jpeg \
+--with-layout=GNU \
+--with-libxml \
+--with-mhash \
+--with-openssl \
+--with-openssl-dir=/usr \
+--without-avif \
+--with-pic \
+--with-readline \
+--with-sodium \
+--with-webp \
+--with-zlib \
+--with-zlib-dir=/usr
+
 ">>$BASE/debian/rules.d/ext-common.mk
 # cat $BASE/debian/rules.d/ext-common.mk; exit 0;
 
