d55b5e847a7a20d24bfabb59026065ca2cd65815
randompage <steamboatid@gmail.com>
2021-10-02 03:20:20 +0700

    autoupdate dkbuild 2021-10-02-03:20:20

--- zargo.sh
+++ zargo.sh
@@ -34,6 +34,7 @@ ssh argo "lxc-start -n eye >/dev/null 2>&1 &"
 # ssh argo -- lxc-attach -n eye -- /bin/bash /tb2/build/dk-prep-net.sh
 
 # ssh argo -- lxc-attach -n bus -- /bin/bash /tb2/build/dk-prep-all.sh
+ssh argo -- lxc-attach -n bus -- /bin/bash /tb2/build/dk-prep-git.sh
 
 # ssh argo -- lxc-attach -n tbus -- /bin/bash /tb2/build/xrepo.sh
 # ssh argo -- /bin/bash /tb2/build/xlast.sh
@@ -49,4 +50,4 @@ ssh argo "lxc-start -n eye >/dev/null 2>&1 &"
 
 # ssh argo "/bin/bash /tb2/build/dk-lxc-exec.sh -n aaa -d buster"
 
-ssh argo -- lxc-attach -n bus -- /bin/bash /tb2/build/5-php.sh
+# ssh argo -- lxc-attach -n bus -- /bin/bash /tb2/build/5-php.sh
