34f821ae7386abcc502e1a50a7b6dedbc9bf20dd
randompage <steamboatid@gmail.com>
2021-10-05 18:37:04 +0700

    autoupdate dkbuild 2021-10-05-18:37:04

--- 6-php.sh
+++ 6-php.sh
@@ -332,10 +332,10 @@ fi
 # cd \.\.\; \\\ \n \
 if [[ $(grep "override_dh_auto_build\:" debian/rules | wc -l) -lt 1 ]]; then
 	DKBUILD="override_dh_auto_build\: \n\
-	pwd\; \\\ \n \
-	for adir in \$(find \.\/ext -mindepth 1 -maxdepth 1 -type d)\; do \\\ \n \
-		cd \$\$adir\; pwd\; \\\ \n \
-		cd \.\.\; \\\ \n \
+	pwd\; \\\ \n\
+	for adir in \$(find \.\/ext -mindepth 1 -maxdepth 1 -type d)\; do \\\ \n\
+		cd \$\$adir\; pwd\; \\\ \n\
+		cd \.\.\; \\\ \n\
 	done \n"
 	sed -i -r "s/\.PHONY/${DKBUILD} \n\.PHONY/g" debian/rules
 fi
