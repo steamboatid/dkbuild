60b91a007aaab08e5f379dfde8f0eb4d22cd7819
randompage <steamboatid@gmail.com>
2021-11-03 08:36:12 +0700

    autoupdate dkbuild 2021-11-03-08:36:12

--- dk-prep-gits.sh
+++ dk-prep-gits.sh
@@ -87,7 +87,7 @@ printf "\n\n --- wait finished... \n\n\n"
 
 # fix raphf
 ln -sf /root/org.src/git-raphf/php_raphf.h /root/org.src/git-raphf/src/php_raphf.h
-ls -lah /root/org.src/git-raphf/php_raphf.h /root/org.src/git-raphf/src/php_raphf.h
+# ls -lah /root/org.src/git-raphf/php_raphf.h /root/org.src/git-raphf/src/php_raphf.h
 ln -sf /root/org.src/git-raphf/src/php_raphf_api.c /root/org.src/git-raphf/php_raphf_api.c
 ln -sf /root/org.src/git-raphf/src/php_raphf_api.h /root/org.src/git-raphf/php_raphf_api.h
 rm -rf /root/org.src/git-raphf/src/php_raphf_test.c
