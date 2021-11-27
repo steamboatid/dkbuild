f58350c4e51022285b6bcc1021fe2a982e3b261c
randompage <steamboatid@gmail.com>
2021-11-27 22:33:01 +0700

    autoupdate dkbuild 2021-11-27-22:33:01

--- zargo.sh
+++ zargo.sh
@@ -29,3 +29,5 @@ ssh argo -- lxca tbus -- ln -sf /tb2/build/dk*sh /usr/local/sbin/
 ssh argo -- lxca teye -- ln -sf /tb2/build/dk*sh /usr/local/sbin/
 
 # ssh argo -- lxc-attach -n eye -- /bin/bash /tb2/build/dk-purge-packages.sh
+
+ssh argo -- lxc-attach -n bus -- /bin/bash /tb2/build/dk-prep-net-php8.sh
