c77ce5a6454ea78f97ee0793e08403b3014d07b4
randompage <steamboatid@gmail.com>
2021-11-28 17:57:40 +0700

    autoupdate dkbuild 2021-11-28-17:57:40

--- dk-build-php8.sh
+++ dk-build-php8.sh
@@ -147,12 +147,14 @@ override_dh_shlibdeps:
 		printf "\n\n\n"
 	fi
 
-	NUMINS=$(ps -e -o command | grep -v grep | grep "dk-build-full" | awk '{print $NF}' | wc -l)
-	if [[ $NUMINS -lt 3 ]]; then
-		doback
-	else
-		if [[ $adir == *"phalcon"* ]]; then doback; else dofore; fi
-	fi
+	# NUMINS=$(ps -e -o command | grep -v grep | grep "dk-build-full" | awk '{print $NF}' | wc -l)
+	# if [[ $NUMINS -lt 3 ]]; then
+	# 	doback
+	# else
+	# 	if [[ $adir == *"phalcon"* ]]; then doback; else dofore; fi
+	# fi
+
+	dofore
 done
 
 
