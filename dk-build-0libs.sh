655513053c36c13d32d954534c4cd706bd3d4800
randompage <steamboatid@gmail.com>
2021-11-28 16:07:42 +0700

    autoupdate dkbuild 2021-11-28-16:07:42

--- dk-build-0libs.sh
+++ dk-build-0libs.sh
@@ -402,7 +402,8 @@ get_update_new_github(){
 
 	if [ ! -d ${DST} ]; then
 		[ ! -z $BRA ] && OPS="-b $BRA" || OPS=""
-		printf "\n---new clone to: $DST \n---from: https://github.com/${URL} $OPS $DST \n"
+		printf "\n---new clone to: $DST \n---from: https://github.com/${URL} $OPS $DST "
+		printf "\n# git clone https://github.com/${URL} $OPS $DST \n"
 		git clone "https://github.com/${URL}" "$OPS" "$DST"
 	else
 		FURL="https://github.com/${URL}"
@@ -418,7 +419,8 @@ get_update_new_gitlab(){
 
 	if [ ! -d ${DST} ]; then
 		[ ! -z $BRA ] && OPS="-b $BRA" || OPS=""
-		printf "\n---new clone to: $DST \n---from: https://gitlab.com/${URL} $OPS $DST \n"
+		printf "\n---new clone to: $DST \n---from: https://gitlab.com/${URL} $OPS $DST "
+		printf "\n# git clone https://gitlab.com/${URL} $OPS $DST \n"
 		git clone "https://gitlab.com/${URL}" "$OPS" "$DST"
 	else
 		FURL="https://gitlab.com/${URL}"
@@ -434,7 +436,8 @@ get_update_new_bitbucket(){
 
 	if [ ! -d ${DST} ]; then
 		[ ! -z $BRA ] && OPS="-b $BRA" || OPS=""
-		printf "\n---new clone to: $DST \n---from: https://bitbucket.org/${URL} $OPS $DST \n"
+		printf "\n---new clone to: $DST \n---from: https://bitbucket.org/${URL} $OPS $DST "
+		printf "\n# git clone https://bitbucket.org/${URL} $OPS $DST \n"
 		git clone "https://bitbucket.org/${URL}" "$OPS" "$DST"
 	else
 		FURL="https://bitbucket.org/${URL}"
@@ -450,7 +453,8 @@ get_update_new_salsa(){
 
 	if [ ! -d ${DST} ]; then
 		[ ! -z $BRA ] && OPS="-b $BRA" || OPS=""
-		printf "\n---new clone to: $DST \n---from: https://salsa.debian.org/${URL} $OPS $DST \n"
+		printf "\n---new clone to: $DST \n---from: https://salsa.debian.org/${URL} $OPS $DST "
+		printf "\n# git clone https://salsa.debian.org/${URL} $OPS $DST \n"
 		git clone "https://salsa.debian.org/${URL}" "$OPS" "$DST"
 	else
 		FURL="https://salsa.debian.org/${URL}"
