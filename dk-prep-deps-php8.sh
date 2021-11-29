66c4bbdc57f5855061b71a287b733784631fdc81
randompage <steamboatid@gmail.com>
2021-11-29 16:00:24 +0700

    autoupdate dkbuild 2021-11-29-16:00:24

--- dk-prep-deps-php8.sh
+++ dk-prep-deps-php8.sh
@@ -161,7 +161,7 @@ while :; do
 	if [[ $aloop -gt 100 ]]; then break; fi
 
 	rets=$(cat $FNOW3 | xargs apt build-dep -my 2>&1)
-	printf "$rets" | grep -i "unable"; printf "$rets" | grep -i "unable" | wc -l
+	# printf "$rets" | grep -i "unable"; printf "$rets" | grep -i "unable" | wc -l
 
 	# anum=$(printf "$rets" | wc -l)
 	# printf "\n --- $anum "; exit 0;
