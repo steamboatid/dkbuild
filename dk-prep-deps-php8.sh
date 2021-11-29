5ee61893ee1f3a8548ac73604b4349062ebe910f
randompage <steamboatid@gmail.com>
2021-11-29 14:44:26 +0700

    autoupdate dkbuild 2021-11-29-14:44:26

--- dk-prep-deps-php8.sh
+++ dk-prep-deps-php8.sh
@@ -148,11 +148,9 @@ apt-cache search php | grep "php\-" | grep "\-dev" | awk '{print $1}' | \
 
 cat $FNOW1 | grep -i "${PHPGREP}\|php\-" | \
 	sort -u | sort | \
-	sed 's/(\([^\)]*\))//g'; exit 0;
-
-cat $FNOW1 | grep -i "${PHPGREP}\|php\-" | \
-	sort -u | sort | \
 	sed 's/(\([^\)]*\))//g' >> $FNOW2
+
+cat $FNOW2; exit 0;
 
 cat $FNOW2 | sort -u | sort | \
 	xargs aptold build-dep -my 2>&1 | tee $FSRC1
