dbb4e2387751f98bb7ebdd6078bf8c32807a42c4
randompage <steamboatid@gmail.com>
2021-12-01 22:57:49 +0700

    autoupdate dkbuild 2021-12-01-22:57:49

--- dk-fix-php-sources.sh
+++ dk-fix-php-sources.sh
@@ -15,6 +15,7 @@ export TODATE=$(date +%Y%m%d)
 
 export PHPVERS=("php8.0" "php8.1")
 export PHPGREP=("php8.0\|php8.1")
+export ERRFIX=0
 
 
 source /tb2/build/dk-build-0libs.sh
@@ -62,62 +63,77 @@ delete_duplicate_dirs(){
 	cd "$odir"
 }
 
+fix_controls_rules(){
+	for adir in $(find /root/src/php -maxdepth 1 -mindepth 1 -type d | grep -v "git-phpredis\|libzip" | sort -nr); do
+		cd $adir
+
+		# temporary solution
+		if [[ $adir == *"http"* ]]; then
+			fix_php_pecl_http "$adir"
+		fi
+		if [[ $adir == *"lz4"* ]]; then
+			fix_php_lz4 "$adir"
+		fi
+		if [[ $adir == *"phalcon3"* ]]; then
+			fix_php_phalcon3 "$adir"
+		fi
+		if [[ $adir == *"-ps-"* ]] && [[ $adir == *"1.4.1"* ]]; then
+			fix_php_ps "$adir"
+		fi
+
+		#---
+		if [[ $adir != *"defaults"* ]] && [[ $adir != *"php8"* ]]; then
+			fix_debian_controls "$adir"
+			/usr/share/dh-php/gen-control
+
+			dhphp4=$(grep "dh\-php (>= " "$adir"/debian/control* | grep " 4" | wc -l)
+			if [[ $dhphp4 -lt 1 ]]; then
+				printf "\n --- $adir ERROR \n"
+				grep "dh\-php (>= " "$adir"/debian/control*
+				printf "\n\n"
+				ERRFIX=$(( $ERRFIX + 1 ))
+			fi
+		fi
+	done
+}
+
+check_missing_controls(){
+	cd /root/src/php
+	for adir in $(find . -mindepth 1 -maxdepth 1 -type d | grep -iv "php8\|xdebug" | sort -nr); do
+		nums=$(find -L "$adir/debian" -iname "control.in" | wc -l)
+		if [[ $nums -lt 1 ]]; then
+			printf "\n -- missing control.in: $adir "
+			ERRFIX=$(( $ERRFIX + 1 ))
+		fi
+	done
+}
+
+
+
+printf "\n\n --- delete duplicates: org.src \n"
+delete_duplicate_dirs "/root/org.src/php"
+delete_duplicate_dirs "/root/org.src/php"
 
 printf "\n\n --- rsync from org.src "
 rsync -aHAXztr --numeric-ids --modify-window 5 --omit-dir-times --delete \
 --exclude ".git" \
 ~/org.src/php/* ~/src/php/
 
-
-
-printf "\n\n --- delete duplicates \n"
-delete_duplicate_dirs "/root/org.src/php"
-delete_duplicate_dirs "/root/org.src/php"
-
+printf "\n\n --- delete duplicates: src \n"
 delete_duplicate_dirs "/root/src/php"
 delete_duplicate_dirs "/root/src/php"
 
 
 printf "\n\n --- fix controls, rules, etc "
-for adir in $(find /root/src/php -maxdepth 1 -mindepth 1 -type d | grep -v "git-phpredis\|libzip" | sort -nr); do
-	cd $adir
+ERRFIX=0
+fix_controls_rules
+if [[ $ERRFIX -gt 0 ]]; then
+	ERRFIX=0
+	fix_controls_rules
+fi
 
-	# temporary solution
-	if [[ $adir == *"http"* ]]; then
-		fix_php_pecl_http "$adir"
-	fi
-	if [[ $adir == *"lz4"* ]]; then
-		fix_php_lz4 "$adir"
-	fi
-	if [[ $adir == *"phalcon3"* ]]; then
-		fix_php_phalcon3 "$adir"
-	fi
-	if [[ $adir == *"-ps-"* ]] && [[ $adir == *"1.4.1"* ]]; then
-		fix_php_ps "$adir"
-	fi
 
-	#---
-	if [[ $adir != *"defaults"* ]] && [[ $adir != *"php8"* ]]; then
-		fix_debian_controls "$adir"
-		fix_debian_controls "$adir"
-		/usr/share/dh-php/gen-control
-
-		dhphp4=$(grep "dh\-php (>= " "$adir"/debian/control* | grep " 4" | wc -l)
-		if [[ $dhphp4 -lt 1 ]]; then
-			printf "\n --- $adir ERROR \n"
-			grep "dh\-php (>= " "$adir"/debian/control*
-			printf "\n\n"
-		fi
-	fi
-
-done
-
-cd /root/src/php
-for adir in $(find . -mindepth 1 -maxdepth 1 -type d | grep -iv "php8\|xdebug" | sort -nr); do
-	nums=$(find -L "$adir/debian" -iname "control.in" | wc -l)
-	if [[ $nums -lt 1 ]]; then
-		printf "\n -- missing control.in: $adir "
-	fi
-done
+printf "\n\n --- check missing controls "
+check_missing_controls
 
 printf "\n\n"
