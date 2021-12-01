90f7404ad1af8aeec850ed7d87d5f12b8f6b0f8b
randompage <steamboatid@gmail.com>
2021-12-01 16:56:44 +0700

    autoupdate dkbuild 2021-12-01-16:56:44

--- dk-build-php8.sh
+++ dk-build-php8.sh
@@ -354,7 +354,7 @@ for adir in $(find /root/src/php -maxdepth 1 -mindepth 1 -type d | grep "http" |
 
 
 	# always do background, avg load already checked in the beginning loop
-	doback "$adir"
+	dofore "$adir"
 	sleep 1
 done
 
