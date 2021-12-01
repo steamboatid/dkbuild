30378f15c21a9a6ecf220a8a44f471a680ed424a
randompage <steamboatid@gmail.com>
2021-12-02 02:32:23 +0700

    autoupdate dkbuild 2021-12-02-02:32:23

--- zargo.sh
+++ zargo.sh
@@ -33,26 +33,9 @@ ssh argo -- lxca bus  -- ln -sf /tb2/build/dk*sh /usr/local/sbin/
 ssh argo -- lxca tbus -- ln -sf /tb2/build/dk*sh /usr/local/sbin/
 ssh argo -- lxca teye -- ln -sf /tb2/build/dk*sh /usr/local/sbin/
 
-# ssh argo -- lxc-attach -n eye -- /bin/bash /tb2/build/dk-purge-packages.sh
-# ssh argo -- lxc-attach -n bus -- /bin/bash /tb2/build/dk-prep-net-nginx.sh
-# ssh argo -- lxc-attach -n bus -- /bin/bash /tb2/build/dk-prep-gits.sh
-
-# ssh argo -- lxc-attach -n bus -- /bin/bash /tb2/build/dk-prep-core-php8.sh
-# ssh argo -- lxc-attach -n bus -- /bin/bash /tb2/build/dk-prep-deps-php8.sh
-
-
 # ssh argo -- lxc-attach -n bus -- /bin/bash /tb2/build/dk-prep-all.sh
-# ssh argo -- lxc-attach -n bus -- /bin/bash /tb2/build/dk-prep-del-dups.sh
+# ssh argo -- lxc-attach -n bus -- /bin/bash /tb2/build/dk-fix-php-sources.sh
+# ssh argo -- lxc-attach -n bus -- /bin/bash /tb2/build/dk-build-php8.sh
+# ssh argo -- lxc-attach -n bus -- /bin/bash /tb2/build/dk-build-check-log.sh
 
-# ssh argo -- lxc-attach -n bus -- /bin/bash /tb2/build/dk-prep-all.sh
-ssh argo -- lxc-attach -n bus -- /bin/bash /tb2/build/dk-fix-php-sources.sh
-ssh argo -- lxc-attach -n bus -- /bin/bash /tb2/build/dk-build-php8.sh
-ssh argo -- lxc-attach -n bus -- /bin/bash /tb2/build/dk-build-check-log.sh
-
-# ssh argo -- lxc-attach -n bus -- /bin/bash /tb2/build/dk-prep-all.sh
-# ssh argo -- lxc-attach -n bus -- /bin/bash /tb2/build/dk-build-all.sh
-
-# ssh argo -- /bin/bash /tb2/build/xrepo-rebuild.sh
-
-# ssh argo -- lxc-attach -n tbus -- /bin/bash /tb2/build/dk-init-debian.sh
-# ssh argo -- lxc-attach -n tbus -- /bin/bash /tb2/build/dk-install-all.sh
+ssh argo "/bin/bash /tb2/build/xbuild-test-all.sh >/dev/null 2>&1 &"
