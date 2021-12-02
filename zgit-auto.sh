5ab3db0d23152a9663d52dd2db765a6c722c9083
randompage <steamboatid@gmail.com>
2021-12-02 11:13:54 +0700

    autoupdate dkbuild 2021-12-02-11:13:54

---
+++ zgit-auto.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+mkdir -p /tb2/root/github/dkbuild/ /tb2/root/github/baks.dkbuild/
+
+rsync -aHAXvztr --numeric-ids --modify-window 5 --omit-dir-times \
+/tb2/root/github/dkbuild/* /tb2/root/github/baks.dkbuild/
+
+rsync -aHAXvztr --numeric-ids --modify-window 5 --omit-dir-times --delete \
+/tb2/build/*sh /tb2/root/github/dkbuild/
+
+rsync -aHAXvztr --numeric-ids --modify-window 5 --omit-dir-times --delete \
+/tb2/build/dk* /tb2/root/github/dkbuild/
+
+cd /tb2/root/github/dkbuild/
+
+for afile in $(find /tb2/root/github/dkbuild/ -mindepth 1 -maxdepth 1 | sort -nr); do
+	bname=$(basename $afile)
+	if [[ "$bname" == ".git"* ]]; then continue; fi
+
+	anum=$(find -L /tb2/build/ -mindepth 1 -maxdepth 1 -iname "${bname}" | wc -l)
+	if [[ $anum -lt 1 ]]; then
+		printf " $bname deleted \n"
+		rm -rf /tb2/root/github/dkbuild/"$bname"
+	fi
+done
+
+git add . >/dev/null 2>&1
+git status -s
+
+git gc --auto --aggressive --prune=now >/dev/null 2>&1
+git gc >/dev/null 2>&1
+git repack -ad >/dev/null 2>&1
+
+git commit -a -m "autoupdate dkbuild `date +%F-%T`"
+git push -u origin master --progress -v
