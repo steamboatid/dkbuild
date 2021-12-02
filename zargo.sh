beb5f59d26c3b95a0562dab9b8858b2d4df427ea
randompage <steamboatid@gmail.com>
2021-12-02 17:20:38 +0700

    autoupdate dkbuild 2021-12-02-17:20:38

--- zargo.sh
+++ zargo.sh
@@ -26,11 +26,11 @@ for alxc in ${lxcs[@]}; do
 	ssh argo -- lxca $alxc -- ln -sf /tb2/build/dk*sh /usr/local/sbin/
 done
 
+# ssh argo -- lxc-attach -n eye -- /bin/bash /tb2/build/dk-build-pcre.sh
 
 # ssh argo -- lxc-attach -n eye -- /bin/bash /tb2/build/dk-prep-all.sh
 ssh argo -- lxc-attach -n eye -- /bin/bash /tb2/build/dk-fix-php-sources.sh
-# ssh argo -- lxc-attach -n eye -- /bin/bash /tb2/build/dk-build-php8.sh
-# ssh argo -- lxc-attach -n eye -- /bin/bash /tb2/build/dk-build-check-log.sh
-# ssh argo -- lxc-attach -n eye -- /bin/bash /tb2/build/dk-build-pcre.sh
+ssh argo -- lxc-attach -n eye -- /bin/bash /tb2/build/dk-build-php8.sh
+ssh argo -- lxc-attach -n eye -- /bin/bash /tb2/build/dk-build-check-log.sh
 
 # ssh argo "/bin/bash /tb2/build/xbuild-test-all.sh >/var/log/dkbuild/build-test-all.log 2>&1 &"
