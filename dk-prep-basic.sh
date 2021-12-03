cd1e3f98bade8af3bb7c2ff6ccf03075fc03fe6e
randompage <steamboatid@gmail.com>
2021-12-03 16:06:17 +0700

    autoupdate dkbuild 2021-12-03-16:06:17

--- dk-prep-basic.sh
+++ dk-prep-basic.sh
@@ -146,12 +146,12 @@ echo \
 
 echo \
 "# deb https://packages.sury.org/php/ ${RELNAME} main
-deb-src https://packages.sury.org/php/ ${RELNAME} main
+# deb-src https://packages.sury.org/php/ ${RELNAME} main
 ">/etc/apt/sources.list.d/php-sury.list
 
 echo \
 "# deb http://repo.aisits.id/php/ ${RELNAME} main
-# deb-src http://repo.aisits.id/php/ ${RELNAME} main
+deb-src http://repo.aisits.id/php/ ${RELNAME} main
 ">/etc/apt/sources.list.d/php-aisits.list
 
 echo \
