9490f67a3f50618a6887f08837023c3471b39381
randompage <steamboatid@gmail.com>
2021-12-04 16:15:33 +0700

    autoupdate dkbuild 2021-12-04-16:15:33

--- zargo.sh
+++ zargo.sh
@@ -36,7 +36,6 @@ done
 
 # ssh argo -- lxc-attach -n eye -- /bin/bash /tb2/build/dk-prep-basic.sh
 # ssh argo -- lxc-attach -n bus -- /bin/bash /tb2/build/dk-prep-basic.sh
-
-# ssh argo "/bin/bash /tb2/build/xbuild-test-all.sh >/var/log/dkbuild/build-test-all.log 2>&1 &"
-
 # ssh argo -- lxc-attach -n eye -- /bin/bash /tb2/build/zdev.sh
+
+ssh argo "/bin/bash /tb2/build/xbuild-test-all.sh >/var/log/dkbuild/build-test-all.log 2>&1 &"
