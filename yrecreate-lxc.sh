f8a8efe25a0a8a0a79028f0244da4e4b1379b1d6
randompage <steamboatid@gmail.com>
2021-12-04 21:02:42 +0700

    autoupdate dkbuild 2021-12-04-21:02:42

--- yrecreate-lxc.sh
+++ yrecreate-lxc.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
 
+export DEBIAN_FRONTEND="noninteractive"
+
+export DEBFULLNAME="Dwi Kristianto"
+export DEBEMAIL="steamboatid@gmail.com"
+export EMAIL="steamboatid@gmail.com"
+
+export RELNAME=$(lsb_release -sc)
+export RELVER=$(LSB_OS_RELEASE="" lsb_release -a 2>&1 | grep Release | awk '{print $2}' | tail -n1)
+
+export TODAY=$(date +%Y%m%d-%H%M)
+export TODATE=$(date +%Y%m%d)
+
+
+source /tb2/build/dk-build-0libs.sh
+
+
 # read command parameter
 #-------------------------------------------
 # while getopts d:y:a: flag
