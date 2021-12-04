4603e298274cb787a4ee057b534136b3985f4392
randompage <steamboatid@gmail.com>
2021-12-05 00:56:25 +0700

    autoupdate dkbuild 2021-12-05-00:56:25

--- zargo.sh
+++ zargo.sh
@@ -32,7 +32,7 @@ done
 # ssh argo -- lxc-attach -n bus -- /bin/bash /tb2/build/dk-fix-php-sources.sh
 # ssh argo -- lxc-attach -n bus -- /bin/bash /tb2/build/dk-build-all.sh
 
-ssh argo -- lxc-attach -n bus -- /bin/bash /tb2/build/dk-prep-core-php8.sh
+# ssh argo -- lxc-attach -n bus -- /bin/bash /tb2/build/dk-prep-core-php8.sh
 ssh argo -- lxc-attach -n bus -- /bin/bash /tb2/build/dk-prep-deps-php8.sh
 # ssh argo -- lxc-attach -n bus -- /bin/bash /tb2/build/dk-build-php8.sh
 # ssh argo -- lxc-attach -n bus -- /bin/bash /tb2/build/dk-build-check-log.sh
