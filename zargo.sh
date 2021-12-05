2d421a687db3cd9051155b3ec544b3f666c9dcae
randompage <steamboatid@gmail.com>
2021-12-05 09:17:16 +0700

    autoupdate dkbuild 2021-12-05-09:17:16

--- zargo.sh
+++ zargo.sh
@@ -1,31 +1,23 @@
 #!/bin/bash
 
 
-# ssh argo "rm -rf /tb2/build/dk-prep*sh"
-
 rsync -aHAXvztr --numeric-ids --modify-window 5 --omit-dir-times \
 /tb2/build/*sh root@argo:/tb2/build/
 
 rsync -aHAXvztr --numeric-ids --modify-window 5 --omit-dir-times \
 /tb2/build/dk* root@argo:/tb2/build/
 
-#-- db4 debs
-# rsync -aHAXvztr --numeric-ids --modify-window 5 --omit-dir-times \
-# /var/lib/lxc/bus/rootfs/root/db4-debs/ argo:/tb2/build/buster-db4/
-
 nohup /bin/bash /tb2/build/zgit-auto.sh >/dev/null 2>&1 &
-# /bin/bash /tb2/build/zgit-auto.sh
-
 ssh argo "nohup chmod +x /usr/local/sbin/* /tb2/build/*sh 2>&1 >/dev/null &"
 
-lxcs=(bus eye tbus teye)
-for alxc in ${lxcs[@]}; do
-	ssh argo -- lxc-stop -kn $alxc
-	ssh argo -- lxc-start -qn $alxc
-	ssh argo -- lxca $alxc -- dhclient eth0 >/dev/null 2>&1 &
-	ssh argo -- lxca $alxc -- rm -rf /usr/local/sbin/dk*sh
-	ssh argo -- lxca $alxc -- ln -sf /tb2/build/dk*sh /usr/local/sbin/
-done
+# lxcs=(bus eye tbus teye)
+# for alxc in ${lxcs[@]}; do
+# 	ssh argo -- lxc-stop -kn $alxc
+# 	ssh argo -- lxc-start -qn $alxc
+# 	ssh argo -- lxca $alxc -- dhclient eth0 >/dev/null 2>&1 &
+# 	ssh argo -- lxca $alxc -- rm -rf /usr/local/sbin/dk*sh
+# 	ssh argo -- lxca $alxc -- ln -sf /tb2/build/dk*sh /usr/local/sbin/
+# done
 
 # ssh argo -- lxc-attach -n eye -- /bin/bash /tb2/build/dk-build-pcre.sh
 
