eb8f381a7862fe02affc2bd67f148ab52990d284
randompage <steamboatid@gmail.com>
2021-12-30 22:33:04 +0700

    autoupdate dkbuild 2021-12-30-22:33:04

--- zargo.sh
+++ zargo.sh
@@ -15,13 +15,14 @@ sleep 0.2
 ssh argo -- killall -9 cc ccache cc1 gcc g++  >/dev/null 2>&1 &
 
 
+printf "\n\n --- prepare lxcs "
+
 lxcs=(bus eye tbus teye)
 for alxc in ${lxcs[@]}; do
-	printf "\n --- prepare lxc: $alxc "
+	printf "\n --- $alxc "
 	ssh argo -- /bin/bash /tb2/build/xrestart-lxc.sh -a "$alxc" >/dev/null 2>&1 &
 done
 
-printf "\n\n --- prepare lxcs "
 wait
 printf "\n\n"
 
