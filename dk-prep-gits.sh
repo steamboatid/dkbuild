dafc66891135b897242f5280a60c296c05f76cd7
randompage <steamboatid@gmail.com>
2022-01-03 11:12:49 +0700

    autoupdate dkbuild 2022-01-03-11:12:49

--- dk-prep-gits.sh
+++ dk-prep-gits.sh
@@ -16,6 +16,11 @@ export TODATE=$(date +%Y%m%d)
 
 source /tb2/build/dk-build-0libs.sh
 
+# check dns resolve
+dig github.com | grep -v ";" | grep "IN"
+dig github.com @172.16.251.1 | grep -v ";" | grep "IN"
+dig github.com @1.1.1.1 | grep -v ";" | grep "IN"
+
 #  kill slow git
 ps axww | grep -v grep | grep git | grep -iv "dk-prep-gits.sh" | awk '{print $1}' | xargs kill -9 >/dev/null 2>&1
 ps axww | grep -v grep | grep git | grep -iv "dk-prep-gits.sh" | awk '{print $1}' | xargs kill -9 >/dev/null 2>&1
