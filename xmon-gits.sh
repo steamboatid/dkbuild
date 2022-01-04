6d8b732d5ebf097ecdd911f1a212b5784d1e4218
randompage <steamboatid@gmail.com>
2022-01-04 21:18:44 +0700

    autoupdate dkbuild 2022-01-04-21:18:44

--- xmon-gits.sh
+++ xmon-gits.sh
@@ -206,7 +206,7 @@ if [[ $sum_delta -gt 0 ]]; then
 	formatted=$(date -u "$format" -d "@$(printf "%010d\n" $sum_delta)" | sed "s|^00:||")
 	printf " -- ${cyn} $formatted ${end} \n\n\n"
 
-	# /bin/bash /tb2/build/xbuild-test-all.sh 2>&1 | tee /var/log/dkbuild/build-test-all.log
+	/bin/bash /tb2/build/xbuild-test-all.sh 2>&1 | tee /var/log/dkbuild/build-test-all.log
 fi
 
 printf "\n\n --- done \n\n"
