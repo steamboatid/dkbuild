67291e18ced1739963255c51c167b34cb2c61914
randompage <steamboatid@gmail.com>
2022-01-04 12:12:28 +0700

    autoupdate dkbuild 2022-01-04-12:12:28

--- zargo.sh
+++ zargo.sh
@@ -38,7 +38,7 @@ echo $MYDIR
 # ssh argo -- lxc-attach -n eye -- /bin/bash /tb2/build/zdev.sh
 
 # ssh argo -- lxc-attach -n eye -- rm -rf /root/org.src/php /root/src/php
-# ssh argo -- lxc-attach -n eye -- /bin/bash /tb2/build/dk-config-gen.sh
+ssh argo -- lxc-attach -n eye -- /bin/bash /tb2/build/dk-config-gen.sh
 ssh argo -- lxc-attach -n eye -- /bin/bash /tb2/build/dk-prep-basic.sh
 # ssh argo -- lxc-attach -n eye -- /bin/bash /tb2/build/dk-prep-core-php8.sh
 # ssh argo -- lxc-attach -n eye -- /bin/bash /tb2/build/dk-prep-deps-nginx.sh
