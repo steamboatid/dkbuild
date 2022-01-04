8c045055c38eb3619878e77230c2cb707d621d31
randompage <steamboatid@gmail.com>
2022-01-04 18:57:03 +0700

    autoupdate dkbuild 2022-01-04-18:57:03

--- zargo.sh
+++ zargo.sh
@@ -46,9 +46,11 @@ echo $MYDIR
 # ssh argo -- lxc-attach -n eye -- /bin/bash /tb2/build/dk-prep-deps-php8.sh
 # ssh argo -- lxc-attach -n eye -- /bin/bash /tb2/build/dk-prep-gits.sh
 
-ssh argo -- lxc-attach -n eye -- /bin/bash /tb2/build/dk-prep-all.sh
+# ssh argo -- lxc-attach -n eye -- /bin/bash /tb2/build/dk-prep-all.sh
 # ssh argo -- lxc-attach -n eye -- /bin/bash /tb2/build/dk-build-sshfs-fuse.sh
 
 # ssh argo -- lxc-attach -n eye -- dig github.com
-# ssh argo -- lxc-attach -n eye -- /bin/bash /tb2/build/dk-prep-gits.sh
 # ssh argo -- lxc-attach -n eye -- /bin/bash /tb2/build/dk-build-db4.sh
+
+ssh argo -- lxc-attach -n eye -- /bin/bash /tb2/build/dk-prep-gits.sh
+ssh argo -- lxc-attach -n eye -- /bin/bash /tb2/build/dk-build-nginx.sh
