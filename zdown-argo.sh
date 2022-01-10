77ccfb4a5fff0cd82997728db5c126226acb8dd8
randompage <steamboatid@gmail.com>
2022-01-10 13:00:48 +0700

    autoupdate dkbuild 2022-01-10-13:00:48

--- zdown-argo.sh
+++ zdown-argo.sh
@@ -11,3 +11,10 @@ root@argo:/tb2/build/* /tb2/build/
 
 rm -rf build bullseye-* buster-* nbproject/
 ls --color=auto -F /tb2/build
+
+# create/update symlinks
+ln -sf /tb2/build/*sh /usr/local/sbin/
+
+# /tb2/build/dk-build-0libs.sh
+# update aptold aptnew
+[[ -f /tb2/build/dk-build-0libs.sh ]] && source /tb2/build/dk-build-0libs.sh
