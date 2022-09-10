72bef028233cdfe01685367fd8f666165fe08a6a
randompage <steamboatid@gmail.com>
2022-09-10 08:58:27 +0700

    autoupdate dkbuild 2022-09-10-08:58:27

--- dk-prep-deps-nginx.sh
+++ dk-prep-deps-nginx.sh
@@ -45,7 +45,7 @@ cd /root/org.src/nginx
 chown -Rf _apt:root /root/org.src/nginx
 
 cat $FDST | grep "Package:" | sed "s/Package\: //g" | \
-grep -iv "resty\|ldap\|brotli\|pam\|js\|redis2" | \
+grep -iv "resty\|ldap\|brotli\|pam\|js\|redis2\|vhost-traffic-status" | \
 tr "\n" " " > $FNOW
 
 cat $FNOW | xargs aptold build-dep -fy
