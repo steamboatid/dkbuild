f38edf0100e093181271044523135cd963ee896d
randompage <steamboatid@gmail.com>
2022-09-17 22:59:44 +0700

    autoupdate dkbuild 2022-09-17-22:59:44

--- xrepo-rebuild.sh
+++ xrepo-rebuild.sh
@@ -75,7 +75,7 @@ mkdir -p /tb2/phideb/dists/{buster,bullseye,bookworm}/main/binary-amd64
 
 # delete old files
 find -L /tb2/phideb -mindepth 2 -type f -delete
-rm -rf /tb2/phideb/pool
+rm -rf /tb2/phideb/pool /tb2/phideb/dists
 
 folders=(php nginx nutcracker lua-resty-core lua-resty-lrucache keydb pcre libzip db4 sshfs)
 for afolder in "${folders[@]}"; do
